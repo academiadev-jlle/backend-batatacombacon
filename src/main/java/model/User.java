7648d7fc771784dad8e395343f8fc61fbf1a52cd
ruanfachini <ruanfachini@linux-u3w0.localdomain>
2018-10-29 12:14:05 -0300

    #12 incluso jpa a lsita de dependencias e inicio do classe User

---
+++ src/main/java/model/User.java
@@ -0,0 +1,53 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	private Long id;
+	@NotNull(message = "Usuário precisa de um nome válido")
+	@Size(min=3,max=120)
+	private String name;
+	private String email;
+	private String password;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
