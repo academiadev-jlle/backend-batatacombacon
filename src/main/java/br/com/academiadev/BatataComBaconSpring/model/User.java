95ed26fb9f46baec369aa3410411a9b5f5696ba1
Allan Krueger <klimber@gmail.com>
2018-10-30 22:00:14 -0300

    Adicionando algumas restrições

--- src/main/java/br/com/academiadev/BatataComBaconSpring/model/User.java
+++ src/main/java/br/com/academiadev/BatataComBaconSpring/model/User.java
@@ -3,6 +3,7 @@ package br.com.academiadev.BatataComBaconSpring.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -14,7 +15,11 @@ public class User {
 	@NotNull(message = "Usuário precisa de um nome válido")
 	@Size(min = 3, max = 120)
 	private String name;
+	@NotNull
+	@Email
 	private String email;
+	@NotNull
+	@Size(min = 6, max = 30)
 	private String password;
 
 	public Long getId() {
