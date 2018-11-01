0b780eadcfa387afd39b5649395419958183ee96
HEMAR <ruanvfachini@gmail.com>
2018-10-31 21:11:56 -0300

    #21 criado repositorio de usuario

---
+++ src/main/java/br/com/academiadev/BatataComBaconSpring/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.academiadev.BatataComBaconSpring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.academiadev.BatataComBaconSpring.model.User;;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
