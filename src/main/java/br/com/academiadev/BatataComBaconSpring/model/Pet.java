dbffd73ec2496efeef3610f31f8d94769969ff40
Allan Krueger <klimber.mail@gmail.com>
2018-11-02 00:51:02 -0300

    Adicionado sexo, localPet, @NotBlank, User

--- src/main/java/br/com/academiadev/BatataComBaconSpring/model/Pet.java
+++ src/main/java/br/com/academiadev/BatataComBaconSpring/model/Pet.java
@@ -1,6 +1,7 @@
 package br.com.academiadev.BatataComBaconSpring.model;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -8,6 +9,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -16,13 +18,7 @@ import org.springframework.lang.Nullable;
 import br.com.academiadev.BatataComBaconSpring.enums.Objetivo;
 import br.com.academiadev.BatataComBaconSpring.enums.Especie;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class Pet {
 
@@ -36,10 +32,14 @@ public class Pet {
 	@ApiModelProperty(name = "Usuário Criador")
 	private User usuario;
 
-	@NotNull
+	@NotBlank
 	@Size(min = 3, max = 30)
 	@ApiModelProperty(example = "Rex", name = "Nome do Pet")
 	private String nome;
+	
+	@NotNull
+	@ApiModelProperty(example = "True", name = "Sexo do pet")
+	private Boolean macho;
 
 	@NotNull
 	@Enumerated(EnumType.STRING)
@@ -50,6 +50,10 @@ public class Pet {
 	@Enumerated(EnumType.STRING)
 	@ApiModelProperty(example = "DOACAO", name = "Objetivo do cadastro deste Pet")
 	private Objetivo objetivo;
+	
+	@NotBlank
+	@ApiModelProperty(example = "Morro da vó Salvelina", name = "Local onde o Pet foi encontrado / perdido / acolhido")
+	private String localPet;
 
 	@NotNull
 	@ApiModelProperty(example = "2018-12-31", name = "Data de cadastro do Pet")
@@ -58,4 +62,77 @@ public class Pet {
 	@Nullable
 	@ApiModelProperty(example = "2018-12-31", name = "Data em que o pet nasceu / foi perdido / foi encontrado")
 	private LocalDate dataPet;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(User usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Boolean getMacho() {
+		return macho;
+	}
+
+	public void setMacho(Boolean macho) {
+		this.macho = macho;
+	}
+
+	public Especie getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(Especie especie) {
+		this.especie = especie;
+	}
+
+	public Objetivo getObjetivo() {
+		return objetivo;
+	}
+
+	public void setObjetivo(Objetivo objetivo) {
+		this.objetivo = objetivo;
+	}
+
+	public String getLocalPet() {
+		return localPet;
+	}
+
+	public void setLocalPet(String localPet) {
+		this.localPet = localPet;
+	}
+
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Optional<LocalDate> getDataPet() {
+		return Optional.ofNullable(dataPet);
+	}
+
+	public void setDataPet(LocalDate dataPet) {
+		this.dataPet = dataPet;
+	}
+
 }
