fc26582a5081eb1673e0503421667a0077461642
Allan Krueger <klimber.mail@gmail.com>
2018-11-04 13:39:29 -0200

    #27 Refatorado segundo lombok
    #27 Adicionado annotations Data, Getter, AllArgsConstructor e NoArgsConstructor nos locais apropriados

--- src/main/java/br/com/academiadev/BatataComBaconSpring/enums/Especie.java
+++ src/main/java/br/com/academiadev/BatataComBaconSpring/enums/Especie.java
@@ -1,16 +1,13 @@
 package br.com.academiadev.BatataComBaconSpring.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Especie {
 	
 	AVE("Ave"), EQUINO("Equino"), CANINO("Canino"), FELINO("Felino"), OUTRO("Outro");
 	
 	private String descricao;
-	
-	private Especie(String descricao) {
-		this.descricao = descricao;
-	}
-	
-	public String getDescricao() {
-		return descricao;
-	}
 }
