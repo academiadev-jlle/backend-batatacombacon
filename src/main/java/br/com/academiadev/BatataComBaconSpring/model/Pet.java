53694b4be6b3db69858e3368a1b0432ad1fa9a8e
Allan Krueger <klimber.mail@gmail.com>
2018-11-08 09:54:09 -0200

    Adicionando auditoria nas classes

--- src/main/java/br/com/academiadev/BatataComBaconSpring/model/Pet.java
+++ src/main/java/br/com/academiadev/BatataComBaconSpring/model/Pet.java
@@ -1,8 +1,10 @@
 package br.com.academiadev.BatataComBaconSpring.model;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
@@ -12,30 +14,32 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.lang.Nullable;
 
 import br.com.academiadev.BatataComBaconSpring.enums.Objetivo;
 import br.com.academiadev.BatataComBaconSpring.enums.Especie;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@ApiModel(description = "Pet")
 public class Pet {
 
 	@Id
 	@GeneratedValue
 	@ApiModelProperty(example = "1", name = "Identificador do Pet")
 	private Long id;
-
-	@NotNull
-	@ManyToOne
-	@ApiModelProperty(name = "Usuário Criador")
-	private User usuario;
 
 	@NotBlank
 	@Size(min = 3, max = 30)
@@ -59,12 +63,23 @@ public class Pet {
 	@NotBlank
 	@ApiModelProperty(example = "Morro da vó Salvelina", name = "Local onde o Pet foi encontrado / perdido / acolhido")
 	private String localPet;
+	
+	@NotNull
+	@ManyToOne
+	@ApiModelProperty(hidden = true)
+	@CreatedBy
+	@Column(name = "created_by", updatable = false)
+	private User usuario;
 
 	@NotNull
-	@ApiModelProperty(example = "2018-12-31", name = "Data de cadastro do Pet")
-	private LocalDate dataCriacao;
+	@ApiModelProperty(hidden = true)
+	@CreatedDate
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
 
 	@Nullable
-	@ApiModelProperty(example = "2018-12-31", name = "Data em que o pet nasceu / foi perdido / foi encontrado")
-	private LocalDate dataPet;
+	@ApiModelProperty(hidden = true)
+	@Column(name = "updated_at", updatable = true)
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
 }
--- src/main/java/br/com/academiadev/BatataComBaconSpring/model/User.java
+++ src/main/java/br/com/academiadev/BatataComBaconSpring/model/User.java
@@ -1,31 +1,61 @@
 package br.com.academiadev.BatataComBaconSpring.model;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@ApiModel(description = "Usuário")
 public class User {
+
 	@Id
 	@GeneratedValue
+	@ApiModelProperty(example = "1", name = "Identificador")
 	private Long id;
-	@NotNull(message = "Usuário precisa de um nome válido")
+
+	@NotBlank(message = "Usuário precisa de um nome válido")
 	@Size(min = 3, max = 120)
+	@ApiModelProperty(example = "João da Silva", name = "Nome")
 	private String nome;
-	@NotNull
+
+	@NotBlank
 	@Email
+	@ApiModelProperty(example = "exemplo@exemplo.com", name = "Email")
 	private String email;
-	@NotNull
+
+	@NotBlank
 	@Size(min = 6, max = 30)
+	@ApiModelProperty(example = "SeperSecreto", name = "Senha")
 	private String senha;
+
+	@CreatedDate
+	@Column(name = "created_at", updatable = false)
+	@ApiModelProperty(hidden = true)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at", updatable = true)
+	@ApiModelProperty(hidden = true)
+	private LocalDateTime updatedAt;
 }
