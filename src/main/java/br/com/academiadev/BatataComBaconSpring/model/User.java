91025802a0dac077d2e07af2e4a0eafb2ce0731a
unknown <ruanfachini>
2018-11-19 21:05:21 -0200

    #4 documentação Swagger classe User

--- src/main/java/br/com/academiadev/BatataComBaconSpring/model/User.java
+++ src/main/java/br/com/academiadev/BatataComBaconSpring/model/User.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,14 +19,18 @@ import lombok.NoArgsConstructor;
 public class User {
 	@Id
 	@GeneratedValue
+    @ApiModelProperty(example = "1" ,name = "Identificador do usuário")
 	private Long id;
 	@NotNull(message = "Usuário precisa de um nome válido")
 	@Size(min = 3, max = 120)
+    @ApiModelProperty(example = "José Silva Figueiredo" ,name = "Nome do usuário")
 	private String nome;
 	@NotNull
 	@Email
+    @ApiModelProperty(example = "petcodes@petcodes.com.br" ,name = "E-mail do usuário")
 	private String email;
 	@NotNull
 	@Size(min = 6, max = 30)
+    @ApiModelProperty(example = "minha senha é essa" ,name = "Senha do usuário")
 	private String senha;
 }
