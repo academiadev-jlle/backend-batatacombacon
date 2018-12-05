dae7057ea45f8b00da5824ebb1668813a647618c
Allan Krueger <klimber.mail@gmail.com>
2018-12-05 21:34:48 -0200

    Adicionando mais 1 header

--- src/main/java/br/com/academiadev/BatataComBaconSpring/config/CustomCORSFilter.java
+++ src/main/java/br/com/academiadev/BatataComBaconSpring/config/CustomCORSFilter.java
@@ -34,7 +34,7 @@ public class CustomCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, accept, content-type");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, accept, content-type, authorization");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
