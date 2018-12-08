a4deaddffeb9293d779072fabe11b1304ec11f87
Allan Krueger <klimber.mail@gmail.com>
2018-12-08 17:44:46 -0200

    Adicionando testes de login

--- src/test/java/br/com/academiadev/BatataComBaconSpring/endpoint/UserEndpointTeste.java
+++ src/test/java/br/com/academiadev/BatataComBaconSpring/endpoint/UserEndpointTeste.java
@@ -1,13 +1,10 @@
 package br.com.academiadev.BatataComBaconSpring.endpoint;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import javax.transaction.Transactional;
 
-import org.assertj.core.api.Assertions;
-import org.junit.Ignore;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -17,7 +14,6 @@ import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -27,52 +23,59 @@ import org.springframework.util.MultiValueMap;
 import br.com.academiadev.BatataComBaconSpring.BatataComBaconSpringApplication;
 import br.com.academiadev.BatataComBaconSpring.repository.UserRepository;
 
-@ActiveProfiles("teste")
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BatataComBaconSpringApplication.class)
-@Ignore
 public class UserEndpointTeste {
-	
-	 @Autowired
-	    private MockMvc mvc;
 
-	    @Value("${security.oauth2.client.client-id}")
-	    private String CLIENT_ID;
+	@Autowired
+	private MockMvc mvc;
 
-	    @Value("${security.oauth2.client.client-secret}")
-	    private String CLIENT_SECRET;
+	@Value("${security.oauth2.client.client-id}")
+	private String CLIENT_ID;
 
-	    @Mock
-	    private UserRepository repository;
-	    
-	    @Test
-	    @Transactional
-	    @Autowired
-	    public void loginTest() throws Exception {
-	        String token = getToken("admin@admin.com", "adminadmin");
-	        Assertions.assertThat(token).isNotNull();
-	    }
-	    
-	    private String getToken(String username, String password) throws Exception {
+	@Value("${security.oauth2.client.client-secret}")
+	private String CLIENT_SECRET;
 
-	        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-	        params.add("grant_type", "password");
-	        params.add("username", username);
-	        params.add("password", password);
+	@Mock
+	private UserRepository repository;
 
-	        ResultActions result
-	                = mvc.perform(post("/oauth/token")
-	                .params(params)
-	                .with(SecurityMockMvcRequestPostProcessors.httpBasic(CLIENT_ID, CLIENT_SECRET))
-	                .accept("application/json;charset=UTF-8"))
-	                .andExpect(status().isOk())
-	                .andExpect(content().contentType("application/json;charset=UTF-8"));
+	@Test
+	@Transactional
+	public void loginTest_UsuarioValido() throws Exception {
+		String token = getToken("admin@batatacombacon.com.br", "AdminComBacon");
+		Assert.assertFalse(token.equals(null));
+	}
 
-	        String resultString = result.andReturn().getResponse().getContentAsString();
+	@Test
+	@Transactional
+	public void loginTest_usuarioInvalido() throws Exception {
+		try {
+			getToken("admin@batatacombacon.com.br", "SenhaErrada");
+			Assert.assertTrue("Deveria dar exceção", false);
+		}catch(Exception e) {
+			Assert.assertTrue("Deveria ser NullPointerException",e instanceof NullPointerException);
+		}
 
-	        JacksonJsonParser jsonParser = new JacksonJsonParser();
-	        return jsonParser.parseMap(resultString).get("access_token").toString();
-	    }
+	}
+
+	private String getToken(String username, String password) throws Exception {
+
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "password");
+		params.add("username", username);
+		params.add("password", password);
+
+		ResultActions result = mvc
+				.perform(post("/oauth/token").params(params)
+						.with(SecurityMockMvcRequestPostProcessors.httpBasic(CLIENT_ID, CLIENT_SECRET))
+						.accept("application/json;charset=UTF-8"));
+//				.andExpect(status().isOk()).andExpect(content().contentType("application/json;charset=UTF-8"));
+
+		String resultString = result.andReturn().getResponse().getContentAsString();
+
+		JacksonJsonParser jsonParser = new JacksonJsonParser();
+		return jsonParser.parseMap(resultString).get("access_token").toString();
+	}
 
 }
